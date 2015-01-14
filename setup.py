59012fc5acd934ce266b60bb00ee89e92fbbc723
Ethan Furman <ethan@stoneleaf.us>
2015-01-13 17:05:26 -0800

    add requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 from distutils.core import setup
+import sys
 
 with open('README') as fh:
     long_desc = fh.read()
 
+if sys.version_info < (3, 4):
+    requirements = ['scription', 'enum34']
+else:
+    requirements = ['scription']
+
 setup( name='xaml',
-       version= '0.1.00',
+       version= '0.1.01',
        license='BSD License',
        description='write XML without writing XML',
        long_description=long_desc,
        py_modules=['xaml', 'xaml_test'],
        provides=['xaml'],
+       install_requires=requirements,
        author='Ethan Furman',
        author_email='ethan@stoneleaf.us',
        url='https://bitbucket.org/stoneleaf.xaml',
