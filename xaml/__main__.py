1c6d446fd1bf1cf44b82d808de9e5a6eafe5473c
Ethan Furman <ethan@stoneleaf.us>
2015-07-17 15:23:44 -0700

    choose extension based on file type

--- xaml/__main__.py
+++ xaml/__main__.py
@@ -8,17 +8,19 @@ Script(
         display=('send output to stdout instead of to DEST', FLAG, None),
         )
 
+
 @Command(
         file=('xaml file to convert to xml', REQUIRED, 'f', Path),
         dest=('name of destination file [default: same name with .xaml -> .xml]', OPTION, 'd', Path),
         same_dir=('create DEST in same directory as FILE [default: current directory]', FLAG),
         )
 def xaml(file, dest, same_dir):
+    "convert FILE to xml/html/css/..."
     if dest is None:
         if file.ext == '.xaml':
-            dest = file.strip_ext() + '.xml'
+            dest = file.strip_ext()
         else:
-            dest = file + '.xml'
+            dest = file
     if not same_dir:
         dest = dest.filename
     with open(file) as source:
@@ -26,21 +28,26 @@ def xaml(file, dest, same_dir):
     if display:
         print(xaml_doc.string(), verbose=0)
     else:
+        dest += '.' + xaml_doc.ml.type
         with open(dest, 'wb') as target:
             target.write(xaml_doc.bytes())
+
 
 @Command(
         file=('xaml file to convert to xml', REQUIRED, 'f', Path),
         )
 def tokens(file):
+    "convert FILE to token format"
     with open(file) as source:
         result = Xaml(source.read(), _compile=False)
     for token in result._tokens:
         print(token, verbose=0)
 
+
 @Command(file=('xaml file to convert', REQUIRED, 'f', Path),
         )
 def code(file):
+    "convert FILE to code used to create final output"
     with open(file) as source:
         result = Xaml(source.read(), _compile=False)
     print(result.document.code, verbose=0)
