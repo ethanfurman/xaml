4900eb1fae85052c296f5991910da66eff99581d
Ethan Furman <ethan@stoneleaf.us>
2015-10-11 20:50:22 -0700

    version 0.4.2

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ if sys.version_info < (3, 4):
     requirements.append('enum34')
 
 setup( name='xaml',
-       version= '0.4.1',
+       version= '0.4.2',
        license='BSD License',
        description='XML Abstract Markup Language',
        long_description=long_desc,
--- xaml/__init__.py
+++ xaml/__init__.py
@@ -14,7 +14,7 @@ import unicodedata
 __all__ = ['Xaml', ]
 __metaclass__ = type
 
-version = 0, 4, 1
+version = 0, 4, 2
 
 module = globals()
 
