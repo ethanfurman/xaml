7d195172df9f9ea06346e1d8144025a0bc6f4e55
Ethan Furman <ethan@stoneleaf.us>
2016-08-15 15:02:28 -0700

    fix version line

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ make = []
 
 data = dict(
        name='xaml',
-       version= '0.5.6',
+       version='0.5.6',
        license='BSD License',
        description='XML Abstract Markup Language',
        long_description=long_desc,
