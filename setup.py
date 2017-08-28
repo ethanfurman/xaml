ffe589370bb618d38bb226f32ee3c9dd7e01dd9c
Ethan Furman <ethan@stoneleaf.us>
2017-08-28 15:36:30 -0700

    version 0.6.1

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ make = []
 
 data = dict(
        name='xaml',
-       version='0.6.0',
+       version='0.6.1',
        license='BSD License',
        description='XML Abstract Markup Language',
        long_description=long_desc,
